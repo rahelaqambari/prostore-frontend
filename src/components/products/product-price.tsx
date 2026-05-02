cdf183752b33a9e4417dc12a60d4275959d4c3df
rahelaqambari <rahelaqambari@gmail.com>
2026-05-02 23:00:41 +0430

    price page for product

---
+++ src/components/products/product-price.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductPrice() {
+  return (
+    <div className='h-screen w-fit flex flex-col-reverse gap-y-px'>
+      
+    </div>
+  )
+}
+
+export default ProductPrice
