8cb2957b14baec4e68ef150b1a19525dcc73ff62
rahelaqambari <rahelaqambari@gmail.com>
2026-05-10 21:31:20 +0430

    working on image

--- src/app/(root)/Products/[id]/page.tsx
+++ src/app/(root)/Products/[id]/page.tsx
@@ -4,13 +4,17 @@ import React from 'react'
 async function DetailsPage({params}:{params: Promise<{id:string}>}) {
   const {id} = await params;
   const products =  await GetProducts();
+  const product = products.data.find((x: any) =>x.id == id);
   return (
     <div className='w-full grid grid-cols-1 md:grid-cols-5 gap-4'>
-      <div>
+      <div className='col-span-2'>
         {/* image */}
       </div>
-      <div>
-        {/* details */}
+      <div className='col-span-2'>
+        <h1>{product.name}</h1>
+        <h1>{product.brand}</h1>
+        <h2>{product.price}</h2>
+        <p>{product.description}</p>
       </div>
       <div>
         {/* cart */}
