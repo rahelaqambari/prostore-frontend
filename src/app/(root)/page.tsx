74fdfbdd96af69440f5be7f91d36e9c43fb5af58
rahelaqambari <rahelaqambari@gmail.com>
2026-06-06 22:07:08 +0430

    solving

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Footer from "../../components/footer";
 import Header from "../../components/shared/header";
-import Hero from "@/components/hero";
 import ProductList from "@/components/shared/products/product-list";
 import { GetProducts } from "@/lib/Actions/product.action";
+import Hero from "@/components/Hero";
 
 export default async function Home() {
   const products = await GetProducts();
   return (
     <div className="">
       <Hero />
-      <ProductList limit={4}  productlist={products} title="Newest Arrivel" />
+      <ProductList limit={4}  productList={products} title="Newest Arrivel" />
     </div>
   );
 }
--- src/components/shared/products/all-product-list.tsx
+++ src/components/shared/products/all-product-list.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import ProductCard from "./product-card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { getAllProducts, getProducts } from "@/lib/actions/product.action";
 import { ProductInfo } from "@/types";
+import { getAllProducts } from "@/lib/Actions/product.action";
+import ProductCard from "./product-card";
 
 function AllProductList({
   limit,
