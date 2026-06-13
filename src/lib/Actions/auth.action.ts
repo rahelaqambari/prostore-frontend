e17c9e764bdbb63e5ab80ad67bc9c5a73747eee0
rahelaqambari <rahelaqambari@gmail.com>
2026-06-13 21:54:32 +0430

    function getuser

--- src/lib/Actions/auth.action.ts
+++ src/lib/Actions/auth.action.ts
@@ -11,6 +11,18 @@ export default async function login(
     });
     const response = await data.json();
     console.log(response);
-   
     return response ;
+}
+
+export async function getUser(token:string) {
+    const data = await fetch(`http://localhost:8000/api/user`,{
+         headers:{
+            accept : "application/json",
+        },
+        method: "POST"
+    });
+    const response = await data.json();
+
+    return response;
+    
 }
