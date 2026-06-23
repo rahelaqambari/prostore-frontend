2c0fb6fc87d03a19ba045e8116e2bffb7f320f4c
rahelaqambari <rahelaqambari@gmail.com>
2026-06-23 15:21:53 +0430

    new changes

--- src/components/shared/dashboard/OverAllReport.tsx
+++ src/components/shared/dashboard/OverAllReport.tsx
@@ -3,10 +3,55 @@ import React from 'react'
 
 function OverAllReport() {
   return (
-    <div className='max-w-6xl bg-white dark:bg-gray-400 w-full p-5 my-4 mx-5'>
-        <div className='border-r border-gray-300'>
+    <div className='max-w-6xl bg-white dark:bg-gray-400 w-full p-5 my-4 mx-5 flex border m-7'>
+        <div className='border-r  border-gray-300 m-6'>
             <div className='flex flex-col space-y-3'>
+                <span className='flex gap-1 items-center dark:text-stone-50 '>
+                    <CircleDollarSign size={18}/>
+                    Monthly Revenue
+                </span>
                 <span>
+                    4000 Previous Month
+                </span>
+                <span className='text-2xl font-bold'>60000</span>
+                <span className='flex gap-1 items-center text-xs'>
+                    <ArrowUpRight size={18} className='text-green-600'/>
+                </span>
+            </div>
+        </div>
+         <div className='border-r  border-gray-300 m-6'>
+            <div className='flex flex-col space-y-3'>
+                <span className='flex gap-1 items-center dark:text-stone-50 '>
+                    <CircleDollarSign size={18}/>
+                    Monthly Revenue
+                </span>
+                <span>
+                    4000 Previous Month
+                </span>
+                <span className='text-2xl font-bold'>60000</span>
+                <span className='flex gap-1 items-center text-xs'>
+                    <ArrowUpRight size={18} className='text-green-600'/>
+                </span>
+            </div>
+        </div>
+         <div className='border-r  border-gray-300 m-6'>
+            <div className='flex flex-col space-y-3'>
+                <span className='flex gap-1 items-center dark:text-stone-50 '>
+                    <CircleDollarSign size={18}/>
+                    Monthly Revenue
+                </span>
+                <span>
+                    4000 Previous Month
+                </span>
+                <span className='text-2xl font-bold'>60000</span>
+                <span className='flex gap-1 items-center text-xs'>
+                    <ArrowUpRight size={18} className='text-green-600'/>
+                </span>
+            </div>
+        </div>
+         <div className='border-r  border-gray-300 m-6'>
+            <div className='flex flex-col space-y-3'>
+                <span className='flex gap-1 items-center dark:text-stone-50 '>
                     <CircleDollarSign size={18}/>
                     Monthly Revenue
                 </span>
