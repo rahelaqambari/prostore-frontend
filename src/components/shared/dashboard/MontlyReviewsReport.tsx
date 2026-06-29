6e6fb2365f41cac23f3658cd7fc9b7a8009c67a6
rahelaqambari <rahelaqambari@gmail.com>
2026-06-29 21:07:24 +0430

    new changes

---
+++ src/components/shared/dashboard/MontlyReviewsReport.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MontlyReviewsReport() {
+  return (
+    <div className='flex flex-col space-y-3'>
+      <h1>The Reviewed Products</h1>
+    </div>
+  )
+}
+
+export default MontlyReviewsReport
